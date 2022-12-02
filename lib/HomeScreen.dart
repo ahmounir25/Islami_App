859ad3b539049eae66de8a888630769f8b9748a0
ahmounir25 <84999077+ahmounir25@users.noreply.github.com>
2022-12-02 19:50:47 +0200

    git init crate the project

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+ static const routeName='home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('islami'),centerTitle: true),
+      //body: ,
+    );
+  }
+}
